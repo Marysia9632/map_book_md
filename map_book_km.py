60e560e6c31036345175b7a23fdfeed3b2bcd77d
Marysia9632 <163434951+Marysia9632@users.noreply.github.com>
2024-04-16 18:14:17 +0200

    wip: add new menu function, add user append

--- map_book_km.py
+++ map_book_km.py
@@ -1,16 +1,19 @@
 from models.data_sources import users
-from untiles.curud import read_friends
+from untiles.curud import read_friends, add_user
 
 
 
 
 if __name__ == '__main__':
     while True:
-        print("Welcome to the menu choose an option: ")
-        print("1. Read a list of friends ")
+        print("Welcome to the menu ")
         print("0. Exit ")
+        print("1. Read a list of friends ")
+        print("2. Add new user ")
         menu_option = input("Choose an option:")
         if menu_option == "0":
             break
-        if menu_option = "1":
+        if menu_option == "1":
             read_friends(users)
+        if menu_option == "2":
+            add_user(users)
--- untiles/curud.py
+++ untiles/curud.py
@@ -2,3 +2,10 @@ def read_friends(users: list)->None:
     print("informacje o twoich znajomych: ")
     for user in users:
         print(f'\tTwój znajomy {user["name"]} {user["surname"]} opublikował {user["posts"]} postów.')
+
+def add_user(lista: list) -> None:
+    imie = input("Podaj imie: ")
+    nazwisko = input("Podaj nazwisko: ")
+    liczba_postów = int(input("Podaj liczbe postów użytkownika: "))
+    new_user = {"name": imie, "surname": nazwisko, "posts": liczba_postów, }
+    lista.append(new_user)
