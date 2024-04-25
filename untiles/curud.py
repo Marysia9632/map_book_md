6381da0fcd96503ace5d4fdfe46d039c57cb032b
Marysia9632 <163434951+Marysia9632@users.noreply.github.com>
2024-04-25 13:48:24 +0200

    feat: add an interactive map

--- untiles/curud.py
+++ untiles/curud.py
@@ -1,32 +1,36 @@
-def read_friends(users: list)->None:
-    print("informacje o twoich znajomych: ")
+def read_friends(users: list) -> None:
+    print("Informacje o twoich znajomych: ")
     for user in users:
         print(f'\tTwój znajomy {user["name"]} {user["surname"]} opublikował {user["posts"]} postów.')
 
+
 def add_user(lista: list) -> None:
-    imie = input("Podaj imie: ")
+    imie = input("Podaj imię: ")
     nazwisko = input("Podaj nazwisko: ")
-    liczba_postów = int(input("Podaj liczbe postów użytkownika: "))
-    new_user = {"name": imie, "surname": nazwisko, "posts": liczba_postów, }
+    liczba_postow = int(input("Podaj liczbę postów użytkownika: "))
+    nazwa_miejscowości = input("Podaj nazwę miejscowości ")
+    new_user = {"name": imie, "surname": nazwisko, "posts": liczba_postow, 'location': nazwa_miejscowości}
     lista.append(new_user)
 
-def search_user(users: list):
-    imie = input("Podaj imie: ")
+
+def search_user(users: list) -> dict:
+    imie = input("Podaj imię: ")
     for user in users:
         if user["name"] == imie:
-            print(user)
+            return user
 
-def remove_user (users: list):
-    imie = input("Podaj imie: ")
+
+def remove_user(users: list):
+    imie = input("Podaj imię: ")
     for user in users:
         if user["name"] == imie:
             users.remove(user)
 
-def update_user (users: list):
-    imie = input("Wprowadż imie użytkowinika, którego dane chcesz zmienić: ")
+
+def update_user(users: list):
+    imie = input("Wprowadź imię użytkownika, którego dane chcesz zmienić: ")
     for user in users:
         if user["name"] == imie:
-            user ["name"] = input("Podaj nowe imię: ")
-            user ["surname"] = input("Podaj nowe nazwisko: ")
-            user ["posts"] = int(input("Podaj liczbę postów: "))
-
+            user["name"] = input("Podaj nowe imię: ")
+            user["surname"] = input("Podaj nowe nazwisko: ")
+            user["posts"] = int(input("Podaj nową liczbę postów: "))
